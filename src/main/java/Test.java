1fc948b97ba094f9afc76e5b31472494dca917e2
wanggf <1362673688@qq.com>
2021-05-13 10:17:25 +0800

    构建项目

---
+++ src/main/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @author Wang Guanfei
+ * @version V2.0.1
+ * @date 2021/5/13 9:49
+ * @Package PACKAGE_NAME
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
